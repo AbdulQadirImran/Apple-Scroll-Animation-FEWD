0e7e73d76f7e38bde7ee53a6750202806ad9c603
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-12 21:45:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const html = document.documentElement;
 const canvas = document.querySelector('.airpod-scrolling');
 const context = canvas.getContext('2d');
+
 const currentFrame = index => (
     `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
